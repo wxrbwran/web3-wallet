068d2ad8aa37510fcc98bc8aad6dc620d264b73c
wuxiaoran <wuxiaoran@wjbrain.com>
2023-07-24 18:53:37 +0800

    feat: 当前投票账户信息

--- src/views/Vote/components/board.vue
+++ src/views/Vote/components/board.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>board</div>
+  <div class="mt-[5px]">
+    <div>
+      <van-cell title="111" icon="shop-o" v-for="(b, i) in data.board" :key="b.targetId + i">
+        <template #right-icon>
+          <van-button @click="vote(i)" size="mini">{{ b.amount }}</van-button>
+        </template>
+      </van-cell>
+    </div>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const data = reactive({
+  board: [
+    {
+      targetId: 1,
+      amount: 1
+    }
+  ]
+})
+const vote = (i: number) => {
+  console.log('vote', i)
+}
+</script>
 <style scoped lang="less"></style>
