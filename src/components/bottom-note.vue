a562b74ba02d0c5d6bdab6d21a372d88bf72c964
wuxiaoran <wuxiaoran@wjbrain.com>
2023-08-11 11:40:52 +0800

    feat: 去掉底部信息

--- src/components/bottom-note.vue
+++ src/components/bottom-note.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="mb-[10px] text-(gray-400 center) animate__animated animate__bounceOutDown">
+  <!-- <div class="mb-[10px] text-(gray-400 center) animate__animated animate__bounceOutDown">
     请确认可以科学浏览
-  </div>
+  </div> -->
+  <div></div>
 </template>
 <style scoped>
 .animate__animated.animate__bounceOutDown {
